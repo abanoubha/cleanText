c617c20fe8d0ec6ff6269d4e7667c688af798155
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-12-24 17:40:24 +0200

    fix: separated cent (small c) and euro (capital C)

--- src/main.go
+++ src/main.go
@@ -58,7 +58,9 @@ func main() {
 				result[i] = 'Z'
 			case 'ž', 'ź', 'ż':
 				result[i] = 'z'
-			case '€', '¢':
+			case '¢':
+				result[i] = 'c' // or '€' : will add a flag '--keep-currency-symbols'
+			case '€':
 				result[i] = 'C' // or '€' : will add a flag '--keep-currency-symbols'
 			case '†', '‡', 'ˆ', '‹', '›', '•', '™':
 				// skip these symbols
