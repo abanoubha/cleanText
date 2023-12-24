cc249ad38bbadff1bb0cf2f6cf5ca6e23a487566
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-12-24 18:28:21 +0200

    feature: more a-like symbols

--- src/main.go
+++ src/main.go
@@ -18,7 +18,7 @@ func main() {
 				result[i] = 'A'
 			case 'Æ':
 				result[i] = 'A' // make it "Ae" later
-			case 'à', 'á', 'â', 'ä', 'ã', 'å', 'ā', 'ª':
+			case 'à', 'á', 'â', 'ä', 'ã', 'å', 'ā', 'ª', 'ą':
 				result[i] = 'a'
 			case 'æ':
 				result[i] = 'a' // make it "ae" later
