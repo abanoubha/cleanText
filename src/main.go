48ba94162ef91655abb309730d65a099a5fb5464
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-12-24 18:40:41 +0200

    feature: more n-like symbols

--- src/main.go
+++ src/main.go
@@ -56,7 +56,7 @@ func main() {
 				result[i] = 'l'
 			case 'Ñ', 'Ń', 'Ņ', 'Ň':
 				result[i] = 'N'
-			case 'ñ', 'ń', 'ņ', 'ň':
+			case 'ñ', 'ń', 'ņ', 'ň', 'ŉ':
 				result[i] = 'n'
 			case 'ô', 'ö', 'ò', 'ó', 'œ', 'ø', 'ō', 'õ', 'º':
 				result[i] = 'o'
