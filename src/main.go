08e9eddbb54735e3ae37677475b92631b5e99bd3
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-12-26 12:51:06 +0200

    feature: convert similar chars

--- src/main.go
+++ src/main.go
@@ -26,6 +26,8 @@ func main() {
 		case "--special-chars", "--special":
 			output := normalizeSpecialChars(inp)
 			fmt.Println(output)
+		case "--similar":
+			fmt.Println(convertSimilarChars(inp))
 		case "-h", "--help":
 			printHelpScreen()
 		default:
@@ -47,4 +49,44 @@ func normalizeSpecialChars(input string) string {
 	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
 	result, _, _ := transform.String(t, input)
 	return result
+}
+
+func convertSimilarChars(input string) string {
+	replacements := map[rune]string{
+		'À': "A",
+		'Á': "A",
+		'Â': "A",
+		'Ä': "A",
+		'Ã': "A",
+		'Å': "A",
+		'Ā': "A",
+		'Ą': "A",
+		'ƛ': "A",
+		'Ǎ': "A",
+		'Ǟ': "A",
+		'Ǡ': "A",
+		'Ǻ': "A",
+		'Ȁ': "A",
+		'Ȃ': "A",
+		'Ȧ': "A",
+		'Ⱥ': "A",
+		'Ʌ': "A",
+
+		'ƙ': "k",
+
+		'Ɖ': "D",
+	}
+
+	// var output []rune
+	output := make([]rune, len(input)) // use less space
+
+	for _, r := range input {
+		if replacement, ok := replacements[r]; ok {
+			output = append(output, []rune(replacement)...)
+		} else {
+			output = append(output, r)
+		}
+	}
+
+	return string(output)
 }
